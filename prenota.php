be99f93c2d1548cfcaad84b104f0c46550e6c7d8
Nizar Nadif <56386611+NizarNadif@users.noreply.github.com>
2021-03-13 11:32:53 +0100

    Added the possibility to see a prenotation

--- prenota.php
+++ prenota.php
@@ -3,11 +3,12 @@
 include_once "config.php";
 
 // variabili valorizzate tramite POST
-$codice_fiscale = $_POST['codice'];
+$codice_fiscale = $_POST['codice_fiscale'];
 $giorno = $_POST['giorno'];
+$codice = strtoupper(uniqid());
 
 // query di inserimento preparata
-$sql = "INSERT INTO prenotazioni VALUES (NULL, :codice_fiscale, :giorno)";
+$sql = "INSERT INTO prenotazioni VALUES (NULL, :codice_fiscale, :giorno, :codice)";
 
 // inviamo la query al database che la tiene pronta
 $stmt = $pdo->prepare($sql);
@@ -16,10 +17,20 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute(
     [
         'codice_fiscale' => $codice_fiscale,
-        'giorno' => $giorno
+        'giorno' => $giorno,
+        'codice' => $codice
     ]
 );
 
-// ridirige il browser verso la pagina indicata nella location
-header('Location:lista_prenotazioni.php');
-exit(0);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prenotazione</title>
+</head>
+<body>
+    <h1>Prenotazione avvenuta con successo</h1>
+    <p>Il codice della tua prenotazione è il seguente: <?php echo $codice; ?></p>
+</body>
+</html>
