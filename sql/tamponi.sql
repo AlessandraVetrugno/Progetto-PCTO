bf41ad3e0983008027c934cf72c3cfeece8805d1
FedericoGavazzi <federico.gavazzi02@gmail.com>
2021-04-22 17:22:39 +0200

    change db schema

--- sql/tamponi.sql
+++ sql/tamponi.sql
@@ -16,6 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `amministratore_presidio`
+--
+
+DROP TABLE IF EXISTS `amministratore_presidio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `amministratore_presidio` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `codice` int(11) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `id_presidio` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `amministratore_presidio_codice_uindex` (`codice`),
+  KEY `amministratore_presidio_presidio_id_fk` (`id_presidio`),
+  CONSTRAINT `amministratore_presidio_presidio_id_fk` FOREIGN KEY (`id_presidio`) REFERENCES `presidio` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `amministratore_presidio`
+--
+
+LOCK TABLES `amministratore_presidio` WRITE;
+/*!40000 ALTER TABLE `amministratore_presidio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `amministratore_presidio` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `amministratore_sistema`
 --
 
@@ -41,56 +70,6 @@ LOCK TABLES `amministratore_sistema` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `amministratore_presidio`
---
-
-DROP TABLE IF EXISTS `amministratore_presidio`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `amministratore_presidio` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `codice` int(11) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `amministratore_presidio_codice_uindex` (`codice`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `amministratore_presidio`
---
-
-LOCK TABLES `amministratore_presidio` WRITE;
-/*!40000 ALTER TABLE `amministratore_presidio` DISABLE KEYS */;
-/*!40000 ALTER TABLE `amministratore_presidio` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `gestisce`
---
-
-DROP TABLE IF EXISTS `gestisce`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `gestisce` (
-  `id` int(11) NOT NULL,
-  `id_prenotazione` int(11) NOT NULL,
-  `id_presidio` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `gestisce`
---
-
-LOCK TABLES `gestisce` WRITE;
-/*!40000 ALTER TABLE `gestisce` DISABLE KEYS */;
-/*!40000 ALTER TABLE `gestisce` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `operatore_sanitario`
 --
 
@@ -102,8 +81,11 @@ CREATE TABLE `operatore_sanitario` (
   `username` varchar(150) NOT NULL,
   `codice` int(11) NOT NULL,
   `password` varchar(45) NOT NULL,
+  `id_presidio` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `operatore_sanitario_codice_uindex` (`codice`)
+  UNIQUE KEY `operatore_sanitario_codice_uindex` (`codice`),
+  KEY `operatore_sanitario_presidio_id_fk` (`id_presidio`),
+  CONSTRAINT `operatore_sanitario_presidio_id_fk` FOREIGN KEY (`id_presidio`) REFERENCES `presidio` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -131,7 +113,10 @@ CREATE TABLE `prenotazione` (
   `eseguito` tinyint(1) DEFAULT 0,
   `note` text DEFAULT NULL,
   `annullato` tinyint(1) DEFAULT 0,
-  PRIMARY KEY (`id`)
+  `id_presidio` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `prenotazione_presidio_id_fk` (`id_presidio`),
+  CONSTRAINT `prenotazione_presidio_id_fk` FOREIGN KEY (`id_presidio`) REFERENCES `presidio` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -156,7 +141,9 @@ CREATE TABLE `presidio` (
   `nome` varchar(100) NOT NULL,
   `via` varchar(45) NOT NULL,
   `id_provincia` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `presidio_provincia_id_fk` (`id_provincia`),
+  CONSTRAINT `presidio_provincia_id_fk` FOREIGN KEY (`id_provincia`) REFERENCES `provincia` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -180,7 +167,9 @@ CREATE TABLE `provincia` (
   `id` int(11) NOT NULL,
   `nome` int(11) NOT NULL,
   `id_regione` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `provincia_regione_id_fk` (`id_regione`),
+  CONSTRAINT `provincia_regione_id_fk` FOREIGN KEY (`id_regione`) REFERENCES `regione` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -225,4 +214,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-19 16:53:18
+-- Dump completed on 2021-04-22 17:20:09
