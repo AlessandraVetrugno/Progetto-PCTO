6b8d7bf41da1b78c538eb9b81898847a84a20a84
FedericoGavazzi <federico.gavazzi02@gmail.com>
2021-05-03 23:29:18 +0200

    add post query

--- server/annulla_prenotazione.php
+++ server/annulla_prenotazione.php
@@ -6,10 +6,11 @@ include_once "config.php";
 $codice_prenotazione = $_POST['codice_prenotazione'];
 
 $sql = "UPDATE prenotazione 
-            SET prenotazione.annullato = true
-            WHERE prenotazione.codice = '$codice_prenotazione'";
-$pdo->query($sql);
+        SET prenotazione.annullato = true
+        WHERE prenotazione.codice = :codice";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['codice'=>$codice_prenotazione]);
 
 // ridirige il browser verso la pagina indicata nella location
-header('Location:prenota');
-exit(0);
+header('Location:./prenota/index.php');
