95c8c23360f23e80a113d744328d2a65e72c74d2
Nizar Nadif <56386611+NizarNadif@users.noreply.github.com>
2021-05-09 23:28:09 +0200

    added image carousel

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,7 +3,10 @@ import { Redirect, Link } from "react-router-dom";
 import "../../assets/styles/home.css";
 import BannerImage from "url:../../assets/img/banner.png";
 import Prenota from "./Prenota";
+import Accedi from "./Accedi";
+import Visualizza from "./Visualizza";
 import Template from '../Template';
+import { Carousel } from 'antd';
 
 export default Home;
 
@@ -15,6 +18,34 @@ function Home(){
 
 function Content() {
 	return (
-			<Prenota />
+		<div className="home">
+			<div className="button-bar">
+				<Prenota />
+				<Visualizza />
+				<Accedi />
+			</div>
+			<CaroselloImmagini />
+		</div>
 	);
+}
+
+function CaroselloImmagini() {
+	return (
+		<div className="carosello">
+			<Carousel autoplay>
+				<div>
+				<h3 className="img-carosello">1</h3>
+				</div>
+				<div>
+				<h3 className="img-carosello">2</h3>
+				</div>
+				<div>
+				<h3 className="img-carosello">3</h3>
+				</div>
+				<div>
+				<h3 className="img-carosello">4</h3>
+				</div>
+			</Carousel>
+		</div>
+	)
 }
