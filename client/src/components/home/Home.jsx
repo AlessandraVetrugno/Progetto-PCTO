baf587657294f1db7cae840ff1125efd8d5c9edd
Nizar Nadif <56386611+NizarNadif@users.noreply.github.com>
2021-05-11 02:15:37 +0200

    dynamic carousel

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { Redirect, Link } from "react-router-dom";
 import "../../assets/styles/home.css";
-import BannerImage from "url:../../assets/img/banner.png";
 import Prenota from "./Prenota";
 import Accedi from "./Accedi";
 import Visualizza from "./Visualizza";
 import Template from '../Template';
 import { Carousel, Image } from 'antd';
-import ImgCarosello1 from "url:../../assets/img/carousel/slide1.jpeg";
-import ImgCarosello2 from "url:../../assets/img/carousel/slide2.jpeg";
-import ImgCarosello3 from "url:../../assets/img/carousel/slide3.jpeg";
-import ImgCarosello4 from "url:../../assets/img/carousel/slide4.jpeg";
+import ImmaginiCarosello from "../../assets/img/carousel/*.jpeg";
 
 export default Home;
 
@@ -34,41 +30,16 @@ function Content() {
 }
 
 function CaroselloImmagini() {
-
-	/* const imgStyle = {
-		height: '100%',
-		width: 'auto'
-	} */
-
-	const imgStyle = {}
-
 	return (
 		<div className="carosello">
 			<Carousel autoplay>
-			{/* <div>
-				<h3 className="img-carosello">1</h3>
-				</div>
-				<div>
-				<h3 className="img-carosello">2</h3>
-				</div>
-				<div>
-				<h3 className="img-carosello">3</h3>
-				</div>
-				<div>
-				<h3 className="img-carosello">4</h3>
-				</div> */}
-				<img className="img-carosello" style={imgStyle}
-					src={ImgCarosello1}
-					/>
-				<img className="img-carosello" style={imgStyle}
-					src={ImgCarosello2}
-					/>
-				<img className="img-carosello" style={imgStyle}
-					src={ImgCarosello3}
-					/>
-				<img className="img-carosello" style={imgStyle}
-					src={ImgCarosello4}
-					/>
+				{
+					Object.entries(ImmaginiCarosello)?.map(
+						immagine => {
+							return <img className="img-carosello" key={immagine[0]} src={immagine[1]} />
+						}
+					)
+				}
 			</Carousel>
 		</div>
 	)
