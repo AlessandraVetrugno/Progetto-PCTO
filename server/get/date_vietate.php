b9509296c2f0dd99299d687e0f84f8e5314e2f47
FedericoGavazzi <federico.gavazzi02@gmail.com>
2021-05-12 12:47:26 +0200

    add date_vietate
    aggiunto il file date_vietate.php e sistemato date_disponibili.php

---
+++ server/get/date_vietate.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once '../config.php';
+
+$response = array();
+$response['status'] = 0;
+
+$req_data = $_GET;
+//prendo l'id del presidio
+$presidio = $req_data['presidio'];
+
+$sql = 'SELECT prenotazione.data, COUNT(*) AS n_prenotazioni
+FROM prenotazione
+WHERE prenotazione.data >= CURDATE() AND prenotazione.id = :id_presidio
+GROUP BY prenotazione.data
+HAVING n_prenotazioni > 5
+ORDER BY prenotazione.data ASC';
+
+//preparo la query
+$stmt = $pdo->prepare($sql);
+
+//eseguo la query
+$stmt->execute([
+    'id_presidio'=>$presidio
+]);
+
+//riempio il vettore di date
+$dates = array();
+while ($row = $stmt -> fetch(PDO::FETCH_ASSOC)) $dates[$row['data']] = $row;
+if($dates != null) {
+    $response['dati'] = datesPrenotable($dates);
+    $response['status'] = 1;
+}
+
+echo json_encode($response);
